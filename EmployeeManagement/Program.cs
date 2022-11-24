5fe38d93d4f78c5d247a6464a0a24f83ba13ad5c
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-24 10:17:33 +0530

    Added program

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EmployeeManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UseCase1
+            EmployeeSalary.EmployeeAttandance();
+            Console.ReadKey();
+            Console.WriteLine("-------------------------");
+            //UseCase2
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.FullTimeEmployeeWage();
+            Console.WriteLine("-------------------------");
+            //UseCase3
+            AddPartTimeEmpWage addPartTimeEmpWage = new AddPartTimeEmpWage();
+            addPartTimeEmpWage.PartTimeEmpWage();
+            Console.WriteLine("-------------------------");
+            //UseCase4
+            CaseStatement caseStatement = new CaseStatement();
+            caseStatement.Display();
+            Console.WriteLine("-------------------------");
+            //UseCase5
+            EmpWagesPerMonth empWagesPerMonth = new EmpWagesPerMonth();
+            empWagesPerMonth.Display();
+            Console.WriteLine("-------------------------");
+            //UseCase6
+            MaxWorkingHours maxWorkingHours = new MaxWorkingHours();
+            maxWorkingHours.Display();
+           
+
+
+        }
+    }
+}
